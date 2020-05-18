f59fcf5b80a621c2e14a121c127793619635c4d2
tkachusik <tkachusik95@gmail.com>
2020-05-19 00:13:06 +0300

    basic form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-lg-8">
+          <p className="msi">MSI 2020</p>
+          <div className="content">
+            <p className="hello">Hey!</p>
+            <p classname="find">Let's try to find a joke for you</p>
+            <div className="form-check form-check-position">
+              <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked />
+              <label className="form-check-label" for="exampleRadios1">
+                Random
+              </label>
+            </div>
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked />
+              <label className="form-check-label" for="exampleRadios1">
+                From categories
+              </label>
+            </div>
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked />
+              <label>
+                Search
+              </label>
+            </div>
+            <div>
+              <button type="button" className="btn btn-primary">Get a joke</button>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-4 favourites min-vh-100">
+        </div>
+      </div>
     </div>
   );
 }
