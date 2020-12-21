7bfd7ad7928a1cbf6baf51cb94e5630921b778ba
tkachusik <tkachusik95@gmail.com>
2020-12-21 11:39:41 +0200

    Updates

---
+++ precache-manifest.600255e37029fec6590e9a5c2365e871.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf298a3a5ef950223d75bca9eddd8c24",
+    "url": "/macpaw-test/index.html"
+  },
+  {
+    "revision": "fff8f7c57c5ac1f82d46",
+    "url": "/macpaw-test/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c46fff51c3d285a4efd8",
+    "url": "/macpaw-test/static/css/main.d9b80d95.chunk.css"
+  },
+  {
+    "revision": "fff8f7c57c5ac1f82d46",
+    "url": "/macpaw-test/static/js/2.e732d94e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/macpaw-test/static/js/2.e732d94e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c46fff51c3d285a4efd8",
+    "url": "/macpaw-test/static/js/main.10e89bc9.chunk.js"
+  },
+  {
+    "revision": "5d30c0515c0a6709accc",
+    "url": "/macpaw-test/static/js/runtime-main.9c0c4c19.js"
+  }
+]);
